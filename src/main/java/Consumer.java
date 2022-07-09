74b10c45734b4be34f3efb12bff59c85ddbcb61b
Vladimir Petrov <ElaijaEc@yandex.ru>
2022-07-09 16:57:02 +0400

    i`m stupid omg

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,39 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+public class Consumer {
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity httpEntity = new HttpEntity<>(null, null);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange("http://94.198.50.185:7081/api/users", HttpMethod.GET, httpEntity, String.class);
+        String cookie = responseEntity.getHeaders().getFirst(httpHeaders.SET_COOKIE);
+        System.out.println(cookie);
+
+        httpHeaders.set(httpHeaders.COOKIE, cookie);
+
+        User user = new User(3L, "James", "Brown", (byte) 26);
+        httpEntity = new HttpEntity<>(user, httpHeaders);
+        System.out.println(httpEntity);
+
+        responseEntity = restTemplate.exchange("http://94.198.50.185:7081/api/users/", HttpMethod.POST, httpEntity, String.class);
+        System.out.println(responseEntity.getBody());
+
+        user = new User(3L, "Thomas", "Shelby", (byte) 26);
+        httpEntity = new HttpEntity<>(user, httpHeaders);
+        responseEntity = restTemplate.exchange("http://94.198.50.185:7081/api/users/", HttpMethod.PUT, httpEntity, String.class);
+        System.out.println(responseEntity.getBody());
+
+        httpEntity = new HttpEntity<>(null, httpHeaders);
+        responseEntity = restTemplate.exchange("http://94.198.50.185:7081/api/users/3", HttpMethod.DELETE, httpEntity, String.class);
+        System.out.println(responseEntity.getBody());
+
+    }
+
+}
